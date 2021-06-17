ee8a96582b99b4b5f4d88f8d62599dfe248d5216
TiernO97 <tiernoyt@gmail.com>
2021-06-18 00:29:21 +0100

    Padding and font changes to cards

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -7,40 +7,40 @@ const Dashboard = () => {
             <CardBody>
                 <Row>
                     <Col lg={3} sm={12}>
-                        <Card className='bg-gradient-to-br from-green-400 to-blue-600 border-none text-light pt-12 sm:mb-2'>
+                        <Card className='bg-gradient-to-br from-green-400 to-blue-600 border-none text-light pt-16 sm:mb-2'>
                             <CardBody>   
                                 <CardText>
-                                    <p className='font-weight-bold'>Students</p>
+                                    <p className='font-semibold text-xl'>Students</p>
                                     7653 students enrolled
                                 </CardText>
                             </CardBody>
                         </Card>
                     </Col>
                     <Col lg={3} sm={12}>
-                        <Card className='bg-gradient-to-br from-red-400 to-purple-600 border-none text-light pt-12 sm:mb-2'>
+                        <Card className='bg-gradient-to-br from-red-400 to-purple-600 border-none text-light pt-16 sm:mb-2'>
                             <CardBody>   
                                 <CardText>
-                                    <p className='font-weight-bold'>Staff</p>
+                                    <p className='font-semibold text-xl'>Staff</p>
                                     45 staff employed
                                 </CardText>
                             </CardBody>
                         </Card>
                     </Col>
                     <Col lg={3} sm={12}>
-                        <Card className='bg-gradient-to-br from-yellow-400 to-red-600 border-none text-light pt-12 sm:mb-2'>
+                        <Card className='bg-gradient-to-br from-yellow-400 to-red-600 border-none text-light pt-16 sm:mb-2'>
                             <CardBody>   
                                 <CardText>
-                                    <p className='font-weight-bold'>Classes</p>
+                                    <p className='font-semibold text-xl'>Classes</p>
                                     <p>17 classes.</p>
                                 </CardText>
                             </CardBody>
                         </Card>
                     </Col>
                     <Col lg={3} sm={12}>
-                        <Card className='bg-gradient-to-br from-red-400 to-red-600 border-none text-light pt-12 sm:mb-2'>
+                        <Card className='bg-gradient-to-br from-red-400 to-red-600 border-none text-light pt-16 sm:mb-2'>
                             <CardBody>   
                                 <CardText>
-                                    <p className='font-weight-bold'>Events</p>
+                                    <p className='font-semibold text-xl'>Events</p>
                                     12 Upcoming Events
                                 </CardText>
                             </CardBody>
